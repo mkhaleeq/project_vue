6a6509af6aa080b5a8d86dd9b1e9ed72a9352dd1
Gr@ndr0am@99 <mkhaleeq28@gmail.com>
2020-08-27 16:18:00 +0530

    updated blog

--- src/components/pages/blog.vue
+++ src/components/pages/blog.vue
@@ -1,6 +1,54 @@
 <template>
      <div>
-        <h1>This is a blog</h1>
+        <div class="blog">
+            <div id="RPLBlog" class="container1">
+                <h3>RPLTechno Blog</h3>
+                <p>Explore this place for great articles and posts of Tech and Buisness</p>
+                <button type="button">Explore More</button>
+            </div>
+            <div class="consult">
+                <h3>Technology Consultation</h3>
+                <p>Explore this place for great articles and posts of Tech and Buisness</p>
+                <button type="button">Sign Up</button>
+            </div>
+            <div class="love">
+                <h3>Love RPLTechno Join Us</h3>
+                <p>Explore this place for great articles and posts of Tech and Buisness</p>
+                <button type="button">Jobs</button>
+            </div>
+            <div class="welcome">
+                <div class="paragraph">
+                    <h3>Welcome To RPL Techno Blog</h3>
+                    <p>You'll find the following on this blog: Latest articles on 
+                        Technology and IT Business Office Updates, Job Openings And 
+                        much more..</p>
+                    </div>
+                     
+                    <div id="react" class="container2">
+                       
+                        <h4>REACT VS ANGULAR WHICH ONE TO CHOOSE ?</h4>
+                        <p>The front-end of the digital product regulates how cosy the users will detect when 
+                            interacting with it. and the product owner has must take care</p>
+                            <a href="#">Read More>></a>
+                                                   
+                    </div>
+
+                    <div id="box" class="blogger">
+                        <h3></h3>
+                         </div>
+                    <div id="angular" class="container3">
+                     <h4>REACT VS ANGULAR WHICH ONE TO CHOOSE ?</h4>
+                        <p>The front-end of the digital product regulates how cosy the users will detect when 
+                            interacting with it. and the product owner has must take care</p>
+                            <a href="#">Read More>></a>
+                            
+                    </div> 
+
+                    <div id="box1" class="blogger1">
+                        <h3></h3>
+                         </div>  
+            </div>
+       </div>
     </div>
 </template>
 <script>
@@ -9,5 +57,293 @@ export default {
 }
 </script>
 <style>
+.blog{
+    top: 0px;
+left: 0px;
+width: 360px;
+height: 76px;
+background: #05103A 0% 0% no-repeat padding-box;
+border: 1px solid #707070;
+opacity: 1;
+}
+#RPLBlog{
+    margin-top:76px;
+  top: 76px;
+left: -156.99998474121094px;
+width: 360px;
+height: 264px;
+background: transparent url('E:/Projects/project_vue/src/assets/blogo.png') 0% 0% no-repeat padding-box;
+opacity: 1;  
+}
+.container1 h3{
+        padding-top:250px ;
+    top: 419px;
+margin-left: 41px;
+width: 152px;
+height: 63px;
+text-align: left;
+font: normal normal bold 28px/32px Ubuntu;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 0.84;
+}
+.container1 p{
+    padding-top:60px;
+  top: 491px;
+margin-left: 41px;
+width: 278px;
+height: 48px;
+text-align: left;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #559BD2;
+opacity: 0.94;  
+}
+.container1 button{
+    margin-top:50px;
+    top: 562px;
+margin-left: 41px;
+width: 165px;
+height: 39px;
+background: #194B84 0% 0% no-repeat padding-box;
+box-shadow: 0px 9px 9px #0000001C;
+border-radius: 4px;
+text-align: center;
+font: normal normal 600 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 0.9;
+
+}
+.consult h3{
+    padding-top:200px;
+   top: 645px;
+    margin-left: 41px;
+width: 172px;
+height: 63px;
+text-align: left;
+font: normal normal bold 28px/32px Ubuntu;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 0.84; 
+}
+.consult p{
+    padding-top:60px;
+    top: 717px;
+margin-left: 41px;
+width: 278px;
+height: 48px;
+text-align: left;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #559BD2;
+opacity: 0.94;
+}
+.consult button{
+    margin-top:50px;
+    top: 788px;
+margin-left: 41px;
+width: 165px;
+height: 39px;
+background: #194B84 0% 0% no-repeat padding-box;
+box-shadow: 0px 9px 9px #0000001C;
+border-radius: 4px;
+text-align: center;
+font: normal normal 600 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 0.9;
+}
+.love h3{
+    padding-top: 40px;
+   top: 871px;
+    margin-left: 41px;
+width: 223px;
+height: 63px;
+text-align: left;
+font: normal normal bold 28px/32px Ubuntu;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 0.84; 
+}
+.love p{
+    padding-top: 40px;
+   top: 943px;
+    margin-left: 41px;
+width: 278px;
+height: 48px;
+text-align: left;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #559BD2;
+opacity: 0.94 
+}
+.love button{
+    margin-top: 40px;
+   top: 1014px;
+margin-left: 41px;
+width: 165px;
+height: 39px;
+background: #194B84 0% 0% no-repeat padding-box;
+box-shadow: 0px 9px 9px #0000001C;
+border-radius: 4px;
+text-align: center;
+font: normal normal 600 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 0.9; 
+}
+.welcome{
+    margin-top: 60px;
+    top: 1109px;
+left: 0px;
+width: 360px;
+height: 1308px;
+background: #05103A 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+.paragraph h3{
+    top: 1133px;
+    padding-top:24px;
+margin-left: 41px;
+width: 278px;
+height: 23px;
+text-align: left;
+font: normal normal bold 20px/24px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 0.84;
+}
+.paragraph p{
+  top: 1173px;
+  padding-top: 27px;
+margin-left: 50px;
+width: 246px;
+height: 106px;
+text-align: center;
+font: normal normal normal 15px/25px Segoe UI;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;  
+}
+#react{ 
+    padding-top: 100px;
+    margin-top:50px; 
+     top: 1312px;
+margin-left: 40px;
+width: 281px;
+height: 171px;
+background: transparent url('E:/Projects/project_vue/src/assets/angular.png') 0% 0% no-repeat padding-box;
+border-radius: 12px;
+opacity: 1; 
+}
+#box{ 
+    top: 1314px;
+margin-left:40px;
+width: 275px;
+height: 280px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 9px 9px #00000029;
+border-radius: 13px;
+opacity: 1;
+}
+.container2 h4{
+    margin-left:18px;
+    margin-top:30px;
+    padding-top: 50px;
+   top: 1520px;
+left: 62px;
+width: 239px;
+height: 75px;
+text-align: left;
+font: normal normal medium 18px/25px Ubuntu;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 1; 
+} 
+.container2 p{
+    margin-left: 18px;
+    margin-top: 60px;
+    top: 1595px;
+left: 61px;
+width: 239px;
+height: 138px;
+text-align: left;
+font: normal normal normal 15px/25px Segoe UI;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 1;
+}
+ .container2 a{
+    
+    top: 1751px;
+margin-left: 90px;
+width: 95px;
+height: 700px;
+text-align: center;
+font: normal normal 600 15px/13px Segoe UI;
+letter-spacing: 0px;
+color: #00A1D4;
+opacity: 1px;
+}
+#angular{
+    padding-top:120px;
+    margin-top:100px;
+    top: 1312px;
+margin-left: 40px;
+width: 281px;
+height: 171px;
+background: transparent url('E:/Projects/project_vue/src/assets/angular.png') 0% 0% no-repeat padding-box;
+border-radius: 12px;
+opacity: 1;
+}
+#box1{
+    top: 1314px;
+    margin-left: 40px;
+left: 43px;
+width: 275px;
+height: 278px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 9px 9px #00000029;
+border-radius: 13px;
+opacity: 1;
+}
+.container3 h4{
+    padding-top:60px;
+    top: 2064px;
+margin-left: 22px;
+width: 239px;
+height: 75px;
+text-align: left;
+font: normal normal medium 18px/25px Ubuntu;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 1;
+}
+.container3 p{
+    top: 2139px;
+    margin-top:65px;
+    margin-left: 22px;
+left: 61px;
+width: 239px;
+height: 138px;
+text-align: left;
+font: normal normal normal 15px/25px Segoe UI;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 1;
+}
+.container3 a{
+    margin-left: 90px;
+    top: 2295px;
+left: 133px;
+width: 95px;
+height: 20px;
+text-align: center;
+font: normal normal 600 15px/13px Segoe UI;
+letter-spacing: 0px;
+color: #00A1D4;
+opacity: 1;
+}
 
 </style>
