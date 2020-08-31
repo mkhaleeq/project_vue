a76f95594ea4c95c4d0c3acf4895bceb38e1610b
Gr@ndr0am@99 <mkhaleeq28@gmail.com>
2020-08-31 14:17:31 +0530

    service page updated

--- src/components/pages/service.vue
+++ src/components/pages/service.vue
@@ -1,4 +1,77 @@
 <template>
+<div>
+    <div id="techno" class="service">
+             <div id="image" class="text">
+                 <button type="button"><a href="#"></a> CONTACT US</button>
+                   </div>
+                   <div id="tech" class="buisness">
+                     <h3>Services From RPL Techno</h3>
+                    <p>RPLTechno is one of the Excellent 
+                         Digital Marketing and Web Designing
+                         Company, We help the Organisation 
+                        to grow their Business through the 
+                         modern techniques by bringing</p>
+                   </div>        
+               <div id="rplserv" class="services">
+            <div class="all">
+                 <h2>All Kind Of Services From RPL Techno</h2>
+                 <p>RPLTechno is one of the Excellent Digital Marketing and Web Designing Company</p>
+                </div>
+            <div id="web" class="devops">
+                <div id="back" class="container1a">
+                <h4>Web Development</h4>
+                </div>
+                <div id="black" class="container1b">
+                <p>We endeavor to provide consumer with every possible digital products and services that helps them save time</p>
+                </div>
+                 </div>
+            <div id="app" class="dev">
+                <div class="container2a">
+                <h4>App Development</h4>
+                </div>
+                <div class="container2b">
+                <p>We endeavor to provide consumer with every possible digital products and services
+                  that helps them save time</p>
+                </div>
+                </div>
+            <div id="digital" class="marketing">
+                <div class="container3a">                    
+                 <h4>Digital Marketing</h4>
+                </div>
+                <div class="container3b">    
+                 <p>We endeavor to provide consumer with every possible digital products and services that helps them save time</p>
+                </div>
+                 </div>
+            <div id="seo" class="manage">
+                <div class="container4a">
+                     <h4>SEO Management</h4>
+                </div>
+                <div class="container4b">
+                     <p>We endeavor to provide consumer with every possible digital products and services that helps them save time</p>
+                </div>
+                 </div>
+            <div id="logo" class="design">
+                <div class="container5a">
+                     <h4>Logo Designing</h4>
+                </div>
+                <div class="container5b">
+                     <p>We endeavor to provide consumer with every possible digital products and services that helps them save time</p>
+                </div>
+                 </div>
+            <div id="design" class="poster">
+                <div class="container6a">
+                <h4>Poster Desining</h4> 
+                </div>
+                <div class="container6b"> 
+                <p>We endeavor to provide consumer with every possible digital products and services that helps them save time</p> 
+                </div>
+                 </div>  
+            
+             </div>            
+
+        </div>
+
+    </div>
     
 </template>
 <script>
@@ -7,5 +80,449 @@ export default {
 }
 </script>
 <style>
-
+*{
+    padding: 0;
+    margin: 0;
+}
+#techno{
+    top: 0px;
+left: 0px;
+width: 300px;
+height: 4140px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+#image{
+    margin-top: 106px;
+    margin-left: 81px;
+width: 197px;
+height: 164px;
+background: transparent url('E:/Projects/project_vue/src/assets/service.png') 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+#tech h3{
+    margin-top: 80px;
+    margin-left: 60px;
+left: 80px;
+width: 250px;
+height: 71px;
+text-align: center;
+font: normal normal bold 31px/36px Ubuntu;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 1; 
+}
+#tech p{
+   margin-left: 33px;
+left: 33px;
+width: 295px;
+height: 140px;
+text-align: center;
+font: normal normal 600 15px/29px Segoe UI;
+letter-spacing: 0px;
+color: #05103A;
+opacity: 1;
+}
+#image button{
+    
+    margin-top: 500px;
+    top: 575px;
+width: 258px;
+height: 46px;
+text-align: center;
+font: normal normal medium 15px/35px Ubuntu;
+letter-spacing: 0px;
+color: #071E3D;
+background: #87FFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 3px 3px #7c89bb4f;
+border-radius: 3px;
+opacity: 1;
+}
+.services{
+    margin-top: 150px;
+   left: 0px;
+width: 360px;
+height: 2920px;
+background: #05103A 0% 0% no-repeat padding-box;
+opacity: 1; 
+}
+.all h2{
+    padding-top: 17px;
+    top: 658px;
+margin-left: 40px;
+width:  282px;
+height: 47px;
+text-align: center;
+font-size: 25px;
+font: normal normal medium 10px/20px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;
+}
+.all p{
+    padding-top: 30px;
+    top: 720px;
+margin-left: 64px;
+width: 233px;
+height: 74px;
+text-align: center;
+font: normal normal normal 15px/25px Segoe UI;
+letter-spacing: 0px;
+color: #AFAFAF;
+opacity: 1;
+}
+.devops{
+    margin-top: 80px;
+    margin-left: 50px;
+    top: 837px;
+margin-left: 29px;
+width: 303px;
+height: 157px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #00000021;
+border-radius: 5px;
+opacity: 1;
+}
+.container1a{
+    margin-left: 80px;
+    top: 844px;
+left: 96px;
+width: 157px;
+height: 151px;
+background: transparent url('E:/Projects/project_vue/src/assets/web.png') 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+.container1a h4{
+    padding-top: 170px;
+    margin-top: 10px;
+    top: 1033px;
+left: 101px;
+width: 155px;
+height: 20px;
+text-align: center;
+font-size: 16px;
+font: normal normal medium 18px/21px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;
+}
+.container1b{
+    margin-top: 5px;
+    top: 837px;
+left: 29px;
+width: 303px;
+height: 180px;
+background: #20263B 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #030A24;
+border-radius: 5px;
+opacity: 1;
+}
+.container1b p{
+    padding-top: 50px;
+    top: 1082px;
+margin-left: 25px;
+width: 252px;
+height: 103px;
+text-align: center;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #A2A2A2;
+opacity: 1;
+}
+.dev{
+    margin-top: 220px;
+    margin-left: 30px;
+    top: 1288px;
+left: 29px;
+width: 303px;
+height: 155px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #00000021;
+border-radius: 5px;
+opacity: 1;
+}
+.container2a{
+    margin-left: 50px;
+    top: 1288px;
+left: 74px;
+width: 184px;
+height: 151px;
+background: transparent url('E:/Projects/project_vue/src/assets/app.png') 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+.container2a h4{
+    padding-top: 165px;
+    padding-left:25px;
+    top: 1934px;
+left: 108px;
+width: 159px;
+height: 20px;
+text-align: center;
+font-size: 16px;
+font: normal normal medium 18px/21px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;
+}
+.container2b{
+    margin-top: 3px;
+    top: 1739px;
+left: 29px;
+width: 303px;
+height: 180px;
+background: #20263B 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #030A24;
+border-radius: 5px;
+opacity: 1;
+}
+.container2b p{
+    padding-top: 45px;
+    padding-left: 30px;
+    top: 1983px;
+left: 55px;
+width: 265px;
+height: 110px;
+text-align: center;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #A2A2A2;
+opacity: 1;
+}
+.marketing{
+    margin-left:30px;
+    top: 1288px;
+left: 29px;
+width: 303px;
+height: 155px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #00000021;
+border-radius: 5px;
+opacity: 1;
+}
+.container3a{
+    margin-top: 250px;
+    top: 1743px;
+left: 31px;
+width: 301px;
+height: 152px;
+background: transparent url('E:/Projects/project_vue/src/assets/marketing.png') 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+.container3b{
+    margin-top: 3px;
+    top: 1739px;
+left: 29px;
+width: 303px;
+height: 180px;
+background: #20263B 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #030A24;
+border-radius: 5px;
+opacity: 1;
+}
+.container3a h4{
+    margin-left: 20px;
+    padding-left: 60px;
+    padding-top: 170px;
+    top: 1934px;
+left: 108px;
+width: 196px;
+height: 20px;
+text-align: center;
+font-size: 16px;
+font: normal normal medium 18px/21px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;
+}
+.container3b p{
+    padding-left: 20px;
+    padding-top:50px;
+    top: 1983px;
+left: 55px;
+width: 272px;
+height: 110px;
+text-align: center;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #A2A2A2;
+opacity: 1;
+}
+.manage{
+    margin-top: 250px;
+    margin-left: 30px;
+    top: 2190px;
+left: 31px;
+width: 301px;
+height: 158px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #00000021;
+border-radius: 5px;
+opacity: 1;
+}
+.container4a{
+    margin-left:60px;
+    top: 2195px;
+left: 91px;
+width: 178px;
+height: 146px;
+background: transparent url('E:/Projects/project_vue/src/assets/seo.png') 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+.container4b{
+    margin-top: 10px;
+        top: 2190px;
+left: 29px;
+width: 303px;
+height: 180px;
+background: #20263B 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #030A24;
+border-radius: 5px;
+opacity: 1;
+}
+.container4a h4{
+    
+    padding-top: 160px;
+    top: 2385px;
+left: 107px;
+width: 158px;
+height: 20px;
+text-align: center;
+font-size: 16px;
+font: normal normal medium 18px/21px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;
+}
+.container4b p{
+    padding-top: 40px;
+    padding-left: 20px;
+    top: 2434px;
+left: 59px;
+width: 260px;
+height: 109px;
+text-align: center;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #A2A2A2;
+opacity: 1;
+}
+.design{
+    margin-top: 250px;
+    margin-left: 30px;
+    top: 2641px;
+left: 31px;
+width: 301px;
+height: 154px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #00000021;
+border-radius: 5px;
+opacity: 1;
+}
+.container5a{
+    margin-left: 60px;
+   top: 2641px;
+left: 106px;
+width: 150px;
+height: 146px;
+background: transparent url('E:/Projects/project_vue/src/assets/logod.png') 0% 0% no-repeat padding-box;
+opacity: 1; 
+}
+.container5b{
+    top: 2641px;
+left: 29px;
+width: 303px;
+height: 185px;
+background: #20263B 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #030A24;
+border-radius: 5px;
+opacity: 1;
+}
+.container5a h4{
+    padding-top:160px;
+    padding-left:20px;
+    top: 2834px;
+left: 116px;
+width: 139px;
+height: 20px;
+text-align: center;
+font-size: 16px;
+font: normal normal medium 18px/21px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;
+}
+.container5b p{
+    padding-top:50px;
+    padding-left:30px;
+    top: 2883px;
+left: 56px;
+width: 270px;
+height: 119px;
+text-align: center;
+font-size: 16px;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #A2A2A2;
+opacity: 1;
+}
+.poster{
+    margin-top: 250px;
+    margin-left: 30px;
+   top: 3092px;
+left: 29px;
+width: 303px;
+height: 155px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #00000021;
+border-radius: 5px;
+opacity: 1; 
+}
+.container6a{
+    
+    margin-left: 60px;
+    top: 3094px;
+left: 103px;
+width: 155px;
+height: 152px;
+background: transparent url('E:/Projects/project_vue/src/assets/poster.png') 0% 0% no-repeat padding-box;
+opacity: 1;
+}
+.container6b{
+    margin-top: 3px;
+    top: 3092px;
+left: 29px;
+width: 303px;
+height: 185px;
+background: #20263B 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 10px #030A24;
+border-radius: 5px;
+opacity: 1;
+}
+.container6a h4{
+    padding-top: 165px;
+    padding-left: 15px;
+    top: 3286px;
+left: 103px;
+width: 145px;
+height: 20px;
+text-align: center;
+font-size: 16px;
+font: normal normal medium 18px/21px Ubuntu;
+letter-spacing: 0px;
+color: #FFFFFF;
+opacity: 1;
+}
+.container6b p{
+    padding-top: 50px;
+    padding-left: 30px;
+    top: 3318px;
+left: 54px;
+width: 265px;
+height: 113px;
+text-align: center;
+font: normal normal normal 18px/24px Segoe UI;
+letter-spacing: 0px;
+color: #A2A2A2;
+opacity: 1;
+}
 </style>
